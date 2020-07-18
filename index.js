a78eb7b6a357c568cfc53ef5e8fde59e368dd62f
ditmarvisser <ditmarvisser@gmail.com>
2020-07-18 16:35:10 +0200

    test with json

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/madick", (req, res) =>
+	res.json({ "1": "Bob's dick is larger than mine!" })
+);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+);
