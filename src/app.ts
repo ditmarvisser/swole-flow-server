1301e5dd0099e9990f185e831a903eec5934b076
ditmarvisser <ditmarvisser@gmail.com>
2020-07-19 15:17:42 +0200

    Changed the dev localhost

--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,5 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(port, () =>
-    console.log(`Server running on port: ${port}`)
+    console.log(`Server running on port: http://localhost:${port}`)
 );
